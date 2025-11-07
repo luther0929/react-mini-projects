279f3bab2d106698d8237b55b874cc3f25d4da3e
Your Name <evangelista.luther1995@gmail.com>
2025-11-07 16:07:30 +1000

    refactor(react-quiz/context): add useQuiz to consume provider for Options component

--- react-quiz/src/components/Options.js
+++ react-quiz/src/components/Options.js
@@ -1,29 +1,40 @@
-function Options({question, dispatch, answer}) {
-    const hasAnswered = answer !== null;
+import { useQuiz } from '../contexts/quizContext';
+import { useEffect } from 'react';
 
-    return (
-        <div className="options">
-            {question.options.map((option, index) =>
-                <button
-                    className={
-                        `btn btn-option
-                        ${index === answer ? "answer" : ""}
-                        ${hasAnswered
-                            ? index === question.correctOption
-                                ? 'correct'
-                                : 'wrong'
+function Options() {
+  const { questions, index, answer, newAnswer, points } = useQuiz();
+  const hasAnswered = answer !== null;
+  const currentQuestion = questions[index];
+
+  // useEffect(() => {
+  //   const logPoints = () => {
+  //     console.log(points);
+  //   };
+  //   logPoints();
+  // }, [newAnswer]);
+
+  return (
+    <div className="options">
+      {currentQuestion.options.map((option, index) => (
+        <button
+          className={`btn btn-option
+                        ${index === answer ? 'answer' : ''}
+                        ${
+                          hasAnswered
+                            ? index === currentQuestion.correctOption
+                              ? 'correct'
+                              : 'wrong'
                             : ''
-                        }`
-                    }
-                    key={option}
-                    disabled={hasAnswered}
-                    onClick={()=>dispatch({ type: 'newAnswer', payload: index })}
-                >
-                    {option}
-                </button>
-            )}
-        </div>
-    );
+                        }`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => newAnswer(index)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default Options;
