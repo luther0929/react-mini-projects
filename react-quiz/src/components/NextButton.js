f039f409507b67c1005c638c71fe944420e55a42
Your Name <evangelista.luther1995@gmail.com>
2025-11-07 16:06:53 +1000

    refactor(react-quiz/context): add useQuiz to consume provider for NextButton component

--- react-quiz/src/components/NextButton.js
+++ react-quiz/src/components/NextButton.js
@@ -1,22 +1,22 @@
-function NextButton({ dispatch, answer, index, numQuestions }) {
-    if(answer === null) return null;
+import { useQuiz } from '../contexts/quizContext';
 
-    if(index < numQuestions - 1) return (
-        <button
-            className="btn btn-ui"
-            onClick={()=>dispatch({ type: "nextQuestion" })}
-        >
-            Next
-        </button>
+function NextButton() {
+  const { answer, index, numQuestions, nextQuestion, finishQuiz } = useQuiz();
+
+  if (answer === null) return null;
+
+  if (index < numQuestions - 1)
+    return (
+      <button className="btn btn-ui" onClick={nextQuestion}>
+        Next
+      </button>
     );
 
-    if(index === numQuestions - 1) return (
-        <button
-            className="btn btn-ui"
-            onClick={()=>dispatch({ type: "finish" })}
-        >
-            Finish
-        </button>
+  if (index === numQuestions - 1)
+    return (
+      <button className="btn btn-ui" onClick={finishQuiz}>
+        Finish
+      </button>
     );
 }
 
