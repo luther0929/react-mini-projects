9d3c29b1a3b944a8839db4d1a5aa347ab7618b86
Your Name <evangelista.luther1995@gmail.com>
2025-11-07 16:08:07 +1000

    refactor(react-quiz/context): add useQuiz to consume provider for Progress component

--- react-quiz/src/components/Progress.js
+++ react-quiz/src/components/Progress.js
@@ -1,15 +1,20 @@
-function Progress({index, numQuestions, points, maxPossiblePoints, answer}) {
-    return (
-        <header className="progress">
-            <progress
-                max={numQuestions}
-                value={index + Number(answer !== null)}
-            />
+import { useQuiz } from '../contexts/quizContext';
 
-            <p>Question <strong>{index + 1}</strong> / {numQuestions}</p>
-            <p><strong>{points}</strong> / {maxPossiblePoints}</p>
-        </header>
-    );
+function Progress() {
+  const { index, numQuestions, points, maxPossiblePoints, answer } = useQuiz();
+
+  return (
+    <header className="progress">
+      <progress max={numQuestions} value={index + Number(answer !== null)} />
+
+      <p>
+        Question <strong>{index + 1}</strong> / {numQuestions}
+      </p>
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints}
+      </p>
+    </header>
+  );
 }
 
 export default Progress;
