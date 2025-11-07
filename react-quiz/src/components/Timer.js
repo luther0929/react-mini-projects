bc0132660360b56fe0ffbef8eb05764ee10bb8f6
Your Name <evangelista.luther1995@gmail.com>
2025-11-07 16:10:05 +1000

    refactor(react-quiz/context): add useQuiz to consume provider for Timer component

--- react-quiz/src/components/Timer.js
+++ react-quiz/src/components/Timer.js
@@ -1,22 +1,17 @@
-import { useEffect } from "react";
+import { useQuiz } from '../contexts/quizContext';
 
-function Timer({dispatch, secondsRemaining}) {
-    const mins = Math.floor(secondsRemaining / 60);
-    const seconds = secondsRemaining % 60;
+function Timer() {
+  const { secondsRemaining } = useQuiz();
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
 
-    useEffect(() => {
-        const id = setInterval(() => {
-            dispatch({ type: 'tick' })
-        }, 1000);
-
-        return () => clearInterval(id);
-    }, [dispatch]);
-
-    return(
-        <div className="timer">
-            {mins < 10 && '0'}{mins}:{seconds < 10 && '0'}{seconds}
-        </div>
-    );
+  return (
+    <div className="timer">
+      {mins < 10 && '0'}
+      {mins}:{seconds < 10 && '0'}
+      {seconds}
+    </div>
+  );
 }
 
 export default Timer;
