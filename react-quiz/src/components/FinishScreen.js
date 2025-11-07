c5602031755c042952624adc2892066406a2ad98
Your Name <evangelista.luther1995@gmail.com>
2025-11-07 16:06:04 +1000

    refactor(react-quiz/context): add useQuiz to consume provider for FinishScreen component

--- react-quiz/src/components/FinishScreen.js
+++ react-quiz/src/components/FinishScreen.js
@@ -1,26 +1,27 @@
-function FinishScreen({maxPossiblePoints, points, highscore, dispatch}) {
-    const percentage = (points / maxPossiblePoints) * 100;
-    let emoji;
-    if(percentage === 100) emoji = '🏅';
-    if(percentage >= 80 && percentage < 100) emoji = '🥈';
-    if(percentage >= 50 && percentage < 80) emoji = '🥉';
-    if(percentage >= 0 && percentage < 50) emoji = '💀';
+import { useQuiz } from '../contexts/quizContext';
 
-    return (
-        <>
-            <p className="result">
-                <span>{emoji}</span> You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
-            </p>
-            <p className="highscore">Highscore: {highscore} points</p>
+function FinishScreen() {
+  const { maxPossiblePoints, points, highscore, restartQuiz } = useQuiz();
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = '🏅';
+  if (percentage >= 80 && percentage < 100) emoji = '🥈';
+  if (percentage >= 50 && percentage < 80) emoji = '🥉';
+  if (percentage >= 0 && percentage < 50) emoji = '💀';
 
-            <button
-                className="btn btn-ui"
-                onClick={()=>dispatch({ type: "restart" })}
-            >
-                Restart quiz
-            </button>
-        </>
-    );
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{' '}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">Highscore: {highscore} points</p>
+
+      <button className="btn btn-ui" onClick={restartQuiz}>
+        Restart quiz
+      </button>
+    </>
+  );
 }
 
 export default FinishScreen;
