e7a7977e495c5083f34bca9e7d0ae59d6998079f
Your Name <evangelista.luther1995@gmail.com>
2025-11-08 23:36:40 +1000

    refactor(atomic-blog/memo): add memo on Archive component to optimize renders

--- atomic-blog/src/App-memo.js
+++ atomic-blog/src/App-memo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext, useContext } from 'react';
+import { useEffect, useState, createContext, useContext, memo } from 'react';
 import { faker } from '@faker-js/faker';
 import { PostProvider, usePosts } from './PostContext';
 
@@ -136,8 +136,7 @@ function List() {
     </ul>
   );
 }
-
-function Archive({ show }) {
+const Archive = memo(function Archive({ show }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
 
   const { onAddPost } = usePosts();
@@ -169,7 +168,7 @@ function Archive({ show }) {
       )}
     </aside>
   );
-}
+});
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
