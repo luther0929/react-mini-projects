272a74037b87e5718c1a99bf454b8ff1e6ba8e13
Your Name <evangelista.luther1995@gmail.com>
2025-11-08 23:51:13 +1000

    refactor(atomic-blog/memo): add archiveOptions object to experiment slowing down renders

--- atomic-blog/src/App-memo.js
+++ atomic-blog/src/App-memo.js
@@ -20,6 +20,11 @@ function App() {
     [isFakeDark]
   );
 
+  const archiveOptions = {
+    show: false,
+    title: 'Post archive in addition to main posts',
+  };
+
   return (
     <section>
       <button
@@ -32,7 +37,7 @@ function App() {
       <PostProvider>
         <Header />
         <Main />
-        <Archive show={false} />
+        <Archive archiveOptions={archiveOptions} />
         <Footer />
       </PostProvider>
     </section>
@@ -136,7 +141,7 @@ function List() {
     </ul>
   );
 }
-const Archive = memo(function Archive({ show }) {
+const Archive = memo(function Archive({ archiveOptions }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
 
   const { onAddPost } = usePosts();
@@ -145,11 +150,11 @@ const Archive = memo(function Archive({ show }) {
     Array.from({ length: 10000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(false);
+  const [showArchive, setShowArchive] = useState(archiveOptions.show);
 
   return (
     <aside>
-      <h2>Post archive</h2>
+      <h2>{archiveOptions.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? 'Hide archive posts' : 'Show archive posts'}
       </button>
