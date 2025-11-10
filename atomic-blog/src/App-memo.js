96c55be17f201f2ac5d8b6fafbcbf3898c9cb209
Your Name <evangelista.luther1995@gmail.com>
2025-11-10 19:49:58 +1000

    refactor(atomic-blog/memo): add useCallback to memoize handAddPost function

--- atomic-blog/src/App-memo.js
+++ atomic-blog/src/App-memo.js
@@ -5,6 +5,7 @@ import {
   useContext,
   memo,
   useMemo,
+  useCallback,
 } from 'react';
 import { faker } from '@faker-js/faker';
 
@@ -35,9 +36,9 @@ function App() {
         )
       : posts;
 
-  function handleAddPost(post) {
+  const handleAddPost = useCallback(function handleAddPost(post) {
     setPosts((posts) => [post, ...posts]);
-  }
+  }, []);
 
   function handleClearPosts() {
     setPosts([]);
@@ -79,7 +80,7 @@ function App() {
 
         <Header />
         <Main />
-        <Archive archiveOptions={archiveOptions} />
+        <Archive archiveOptions={archiveOptions} onAddPost={handleAddPost} />
         <Footer />
       </section>
     </PostContext.Provider>
@@ -184,10 +185,10 @@ function List() {
   );
 }
 
-const Archive = memo(function Archive({ archiveOptions }) {
+const Archive = memo(function Archive({ archiveOptions, onAddPost }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
 
-  const { onAddPost } = useContext(PostContext);
+  // const { onAddPost } = useContext(PostContext);
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
