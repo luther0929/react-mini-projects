eb5ccc3bdf12212517e566fbcc36c5b469f7bff7
Your Name <evangelista.luther1995@gmail.com>
2025-11-14 20:48:00 +1000

    refactor(redux-intro/redux): add accountSlice for better structure

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,43 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 
-const initialStateAcocunt = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: '',
-};
-
 const initialStateCustomer = {
   fullName: '',
   nationalID: '',
   createdAt: '',
 };
-
-function accountReducer(state = initialStateAcocunt, action) {
-  switch (action.type) {
-    case 'account/deposit':
-      return { ...state, balance: state.balance + action.payload };
-    case 'account/withdraw':
-      if (action.payload > state.balance) return state;
-      return { ...state, balance: state.balance - action.payload };
-    case 'account/requestLoan':
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case 'account/payLoan':
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: '',
-        balance: state.balance - state.loan,
-      };
-    default:
-      return state;
-  }
-}
 
 function customerReducer(state = initialStateCustomer, action) {
   switch (action.type) {
