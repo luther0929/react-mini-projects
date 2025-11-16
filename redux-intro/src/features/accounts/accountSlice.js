01f96b91a4f4d318d4f79dd20e1aab2c0c5eb829
Your Name <evangelista.luther1995@gmail.com>
2025-11-16 14:37:58 +1000

    refactor(redux-intro/currency): implement again currency conversion logic with thunks

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -13,6 +13,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance += +action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
@@ -36,12 +37,31 @@ const accountSlice = createSlice({
       state.loan = 0;
       state.loanPurpose = '';
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 
-console.log(accountSlice);
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export function deposit(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+
+  return async function (dispatch, getState) {
+    //API call
+    dispatch({ type: 'account/convertingCurrency' });
+
+    const res = await fetch(
+      `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=USD`
+    );
+    const data = await res.json();
+    const convertedAmount = Number((amount * data.rates['USD']).toFixed(2));
+
+    // return action
+    dispatch({ type: 'account/deposit', payload: convertedAmount });
+  };
+}
 
 export default accountSlice.reducer;
 
